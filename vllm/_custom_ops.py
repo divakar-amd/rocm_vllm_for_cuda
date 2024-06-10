cf1d37c8b0dac886448c5fdcd7045064aae78cd2
Divakar Verma <divakar.verma@amd.com>
2024-06-10 22:58:57 +0000

    Revert "adding rocm fp8"
    This reverts commit bfb06672c49e30ca5b39804d950fad22008e2c0d.

--- vllm/_custom_ops.py
+++ vllm/_custom_ops.py
@@ -328,8 +328,9 @@ def swap_blocks(src: torch.Tensor, dst: torch.Tensor,
 
 def convert_fp8(output: torch.Tensor,
                 input: torch.Tensor,
-                scale: float = 1.0) -> None:
-    vllm_ops.convert_fp8(output, input, torch.Tensor([scale]))
+                scale: float = 1.0,
+                kv_dtype: str = "fp8") -> None:
+    vllm_cache_ops.convert_fp8(output, input, scale, kv_dtype)
 
 
 #TODO: cuda_utils, custom_ar
